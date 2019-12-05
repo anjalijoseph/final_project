421eefcdbba622090c622dd01b90d0a7a95a7333
Raghu Betina <raghu@firstdraft.com>
2019-12-05 20:59:41 +0000

    Whitelist columns in HotelReview for ActiveAdmin

--- app/admin/hotel_reviews.rb
+++ app/admin/hotel_reviews.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register HotelReview do
+
+ permit_params :hotel_id, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
